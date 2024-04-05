f933657afd0c488b186756778e032ce3bf599ab1
eunjinnnn <sally0680@naver.com>
2024-04-05 18:02:20 +0900

    change post_list templates

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Post #.model은 현재 파일에 있다의미
+from django.utils import timezone
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')#쿼리셋
+    return render(request, 'blog/post_list.html', {'posts': posts})
