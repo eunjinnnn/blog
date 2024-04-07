cc99af00c2482d74ecab057d65648217ae7c0746
eunjinnnn <sally0680@naver.com>
2024-04-07 23:30:51 +0900

    어플리케이션 확장하기

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Post #.model은 현재 파일에 있다의미
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
-def post_list(request):
-    posts = Post.objects.all()#쿼리셋
-    return render(request, 'blog/post_list.html', {'posts': posts})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
