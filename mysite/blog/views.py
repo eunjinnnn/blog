07adfdba5232265a94b30e3202aa1d14dfee81ac
eunjinnnn <sally0680@naver.com>
2024-04-10 21:38:17 +0900

    blog.css 적용이안됨

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
-    posts = Post.objects.filter(
-        published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    #궁금한점.. 왜 전부다 안나오고 second만 나오지? 이해가안감 
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
